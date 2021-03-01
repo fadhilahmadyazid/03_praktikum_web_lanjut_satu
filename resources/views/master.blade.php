ebc31338ffca0892f708f7f71a986bbb0351fb18
fadhil <fadhilahmadyazid@gmail.com>
2021-03-01 18:09:26 +0700

    masterview

---
+++ resources/views/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> Halaman @yield('title')</title>
+  <meta name="description" content="">
+<!-- 
+Motor Template
+http://www.templatemo.com/tm-463-motor
+-->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/templatemo-style.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+      </head>
+
+      <body>@section( 'header')
+      Ini adalah master header.
+      @show
+      @section('sidebar')
+      Ini adalah master sidebar.
+      @show
+      <div class="container">
+      @yield('content')
+      </div>
+      <footer> @yield('footer') </footer>
+      </body></html>
